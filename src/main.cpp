58de4e4917ff9e98cdf777209e57aa64f74ebe21
Skyggebjerg <ole@skyggebjerg.dk>
2024-01-30 00:47:47 +0100

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,8 @@
 #include <Arduino.h>
 
-
-  /*
-  Read the 18 channels of spectral light over I2C using the Spectral Triad
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: October 25th, 2018
-  License: MIT. See license file for more information but you can
-  basically do whatever you want with this code.
-
-  This example shows how to output the raw sensor values. This is probably never needed since the 
-  calibrated values are tuned to each sensor. But it does run faster (2 bytes per channel instead of 4)
-  
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun! https://www.sparkfun.com/products/15050
-
-  Hardware Connections:
-  Plug a Qwiic cable into the Spectral Triad and a BlackBoard
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
-*/
 #include <M5Stack.h>
 #include "SparkFun_AS7265X.h" //Click here to get the library: http://librarymanager/All#SparkFun_AS7265X
 AS7265X sensor;
-
-//#include <Wire.h>
 
 void setup()
 {
